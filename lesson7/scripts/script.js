58120cee41658fb28a86140f6ec08f667c003504
dato201 <146935809+dato201@users.noreply.github.com>
2023-12-11 17:59:21 +0400

    add lesson7 finished

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -6,6 +6,7 @@ let BlueButton = document.getElementById("Blue_button");
 let GreenButton = document.getElementById("Green_button");
 let body = document.querySelector("body");
 let buttonSeeEndHide = document.getElementById("but_007");
+let buttonSeeEndHide2 = document.getElementById("but_008");
 let title = document.getElementById("h1_1");
 let article = document.getElementById("lorem");
 let article2 = document.getElementById("lorem2");
@@ -13,14 +14,13 @@ let article2 = document.getElementById("lorem2");
 
 
 function minusImg() {
-    img.style.width = "-10%";
-    img.style.height = "-10%";
+    img.style.width = "200px";
+    img.style.height = "150px";
 }
 
-
 function plusImg() {
-    img.style.width = "10%";
-    img.style.height = "10%";
+    img.style.width = "550px";
+    img.style.height = "400px";
 }
 
 function RedBoody() {
@@ -35,93 +35,30 @@ function GreenBoody() {
     body.style.backgroundColor = "green";
 }
 
+title.style.display = "none";
+article.style.display = "none";
+article2.style.display = "none";
+
+buttonSeeEndHide2.style.display = "none";
 function buttonSeeHide() {
-    if (buttonSeeEndHide = "ჩვენება") {
-        title.innerHTML = `
-        <h1 id="h1_1">statia</h1>
-        ` 
-        article.innerHTML = `
-        <p id="lorem">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt similique atque, ut placeat dolor
-        repellat fuga praesentium excepturi modi ducimus doloribus voluptatem explicabo nisi. Iusto quo dolorum laborum
-        quam Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas ea hic ipsum aperiam ab nihil quibusdam
-        ipsam esse laboriosam a sed, ut id beatae vitae itaque minima, iure necessitatibus ducimus? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Aut autem aliquam repellat excepturi iure illum quibusdam minus,
-        inventore optio, doloribus, perspiciatis eos? Dicta, eaque perferendis! Ea explicabo laudantium voluptates quam?
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam voluptates, ducimus minima, natus repellat cum
-        repudiandae quibusdam molestias rem vel iste, illum tempore quasi voluptatibus deleniti ea iure facere dolor!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias recusandae totam error asperiores
-        quae ipsam hic vero natus quod! Architecto nemo similique doloremque totam officia eveniet laborum consequuntur
-        quasi vero. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit pariatur, natus ex sapiente velit
-        saepe enim ea nam laboriosam? Et ea numquam dolor enim officiis atque reprehenderit eligendi rerum nisi? Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Beatae necessitatibus dicta cupiditate tempore, reprehenderit
-        assumenda dolorem adipisci rem consequuntur quos ab nihil incidunt molestias modi? Cupiditate molestiae mollitia
-        totam temporibus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse dolore iusto veniam fugiat
-        quibusdam labore obcaecati, nesciunt nobis dolor sint a in! Consequatur, veniam cumque fugiat architecto
-        dignissimos expedita sit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel asperiores repudiandae
-        nesciunt fugit, delectus iure, vitae animi quibusdam perspiciatis aperiam libero laudantium quod alias! Qui
-        ipsum dolor culpa beatae. Quo! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident voluptatum
-        atque alias incidunt impedit, rerum ex quae deserunt rem sequi, a ab vero dolore distinctio commodi laborum
-        nobis quaerat veritatis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores minima, est non cumque
-        atque provident eos illo iste natus corporis sit blanditiis cum laborum ut! Dolorem dolorum commodi rerum
-        inventore? Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa esse ad quibusdam aperiam reiciendis,
-        hic sequi labore maxime fugiat neque, laudantium, mollitia in totam perspiciatis accusantium reprehenderit
-        suscipit ut sit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolorum beatae soluta
-        consectetur veniam alias ea ab, quis officiis quibusdam voluptas ad tempora dolor sapiente odit, architecto
-        aliquam.
-        Magni, mollitia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur numquam quas assumenda, laborequos
-        vero odit cupiditate veniam officiis expedita ipsa iusto accusantium aliquam corrupti iste doloribus quibusdam
-        id? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore harum accusamus minima delectus ad dolorem
-        veniam nulla animi quia saepe? Blanditiis consequuntur dolor voluptate nulla at sit in iusto expedita. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Ipsum, accusantium aliquam. Architecto inventore rerum
-        numquam porro reiciendis dolores beatae alias consectetur. Obcaecati deleniti aliquam eaque quidem pariatur
-        beatae eius ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum omnis amet culpa doloribus
-        dolorem soluta temporibus iste, nobis voluptates adipisci reiciendis officiis aut quas magni sint, doloremque
-        sit? Esse, deleniti. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, alias voluptates
-        officiis odit quod blanditiis possimus nihil recusandae quidem sunt libero reiciendis modi autem quasi. Modi
-        nesciunt id tempora. Culpa. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, incidunt laborum
-        animi sunt voluptatem officiis consequuntur earum ab est vero fugiat, repellendus ad? Tenetur ab totam mollitia
-        omnis a quia? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, libero ab. Sunt ad ullam
-        perspiciatis quasi corporis aut ea magnam veritatis? Earum totam minima harum facilis suscipit ratione iste
-        doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quam eum optio harum quas in
-        voluptate, corrupti doloremque! Provident at quisquam est ad aspernatur itaque perspiciatis quaerat culpa
-        excepturi soluta. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem a corrupti officiis
-        tempora fuga quasi! Libero ab deserunt ad adipisci deleniti doloremque iste, temporibus, debitis enim, optio
-        ipsam quaerat! Ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident inventore sed
-        exercitationem consequatur numquam vero sint culpa non molestiae rem, tenetur vel nemo. Asperiores molestias,
-        earum explicabo adipisci quia saepe? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi delectus
-        voluptate iusto. Dicta cupiditate quos autem voluptatem corporis beatae, delectus obcaecati libero eius commodi
-        veritatis quas eum illo unde quasi.
-        `
-        article2.innerHTML = `
-        <p id="lorem2">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim illum quos necessitatibus animi asperiores cumque
-        totam eveniet quod tempore, labore fugiat non dolorem minus beatae sint natus dicta iure consequuntur! Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Sed mollitia ex earum dolorum, incidunt eos veritatis
-        corporis. Consequatur animi sapiente nesciunt, sunt illo minima ut laborum, ex quam sequi consectetur. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Eos, dolorem cumque? A officiis harum labore saepe blanditiis
-        inventore iste itaque vero, tempora ipsam dicta, ea beatae voluptas, perspiciatis repellendus quis? Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Eligendi mollitia, cum possimus aliquam iure modi, tenetur pariatur
-        vero nobis, est itaque officia placeat quam saepe delectus libero id! Omnis, quas? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Illum maiores nihil dolorem facilis molestiae tenetur ipsam, ipsa possimus ullam,
-        dolor quia! Incidunt officia itaque iste quia recusandae perspiciatis quasi molestias?
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati unde hic deserunt sit cum commodi inventore
-        ipsam ducimus mollitia modi autem, aut assumenda eum, eaque molestias accusamus voluptatibus perferendis quam?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus obcaecati commodi harum accusamus debitis
-        quod voluptatibus ut earum ad odit totam architecto, libero atque tempora repellendus possimus nesciunt aperiam
-        blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptate tenetur dignissimos
-        eligendi vitae recusandae pariatur officiis, consequatur amet! Alias cum nesciunt nisi ab sapiente voluptatibus
-        quasi adipisci, veritatis quibusdam? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda soluta
-        cumque dolore nulla commodi tempore similique voluptatem illum obcaecati. Recusandae, voluptatem perferendis
-        dolore illum ea ipsum architecto magnam porro odit. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Animi mollitia sunt quo? Delectus adipisci, fugit deserunt earum laudantium hic sed cum architecto asperiores.
-        Explicabo officia cupiditate nisi, reiciendis minus ipsam. Lorem ipsum dolor sit, amet consectetur adipisicing
-        elit. Omnis totam voluptatem harum minus suscipit repellat iusto voluptatibus nemo nisi nesciunt natus, eos
-        neque dolorem rerum est amet magnam, fugit odio.
-        </p>
-        `
-        buttonSeeEndHide.innerHTML = "დამალვა";
-    }    
+    buttonSeeEndHide.style.display = "none";
+
+    buttonSeeEndHide2.style.display = "block";
+
+    title.style.display = "block";
+
+    article.style.display = "block";
+
+    article2.style.display = "block";
 }
 
+function buttonSeeHide2() {
+    buttonSeeEndHide.style.display = "block";
+    buttonSeeEndHide2.style.display = "none";
 
+    title.style.display = "none";
+
+    article.style.display = "none";
+
+    article2.style.display = "none";
+}
