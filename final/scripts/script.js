70f0fe8b88678e61ba5ebd0c988d5af686aef75f
dato201 <146935809+dato201@users.noreply.github.com>
2024-01-14 21:04:21 +0400

    add final dan

---
+++ final/scripts/script.js
@@ -0,0 +1,11 @@
+function LoginButton() {
+    let backgroundLogin = document.getElementById("backgroundLogin");
+    let loginWindow = document.getElementById("loginWindow");
+
+    backgroundLogin.className = "backgroundLogin";
+    loginWindow.innerHTML = `
+    <div id="loginWindow">
+
+    </div>
+    `
+}
