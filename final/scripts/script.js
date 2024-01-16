aee1a0ca86a1ce8f373f3301f229e071ea8c9ad5
dato201 <146935809+dato201@users.noreply.github.com>
2024-01-16 22:28:07 +0400

    add final dan

--- final/scripts/script.js
+++ final/scripts/script.js
@@ -3,24 +3,28 @@ function LoginButton() {
 
     backgroundLogin.innerHTML = `
     <div id="backgroundLogin" onclick="xmark()" class="backgroundLogin">
-    <div id="loginWindow" class="login_window">
-        <button class="x" onclick="xmark()"><i class="fa-solid fa-xmark"></i></button>
-        <form class="login_window_form">
-            <input type="text">
-            <input type="text">
-        </form>
+        <div id="loginWindow" class="login_window">
+            <button class="x" onclick="xmark()"><i class="fa-solid fa-xmark"></i></button>
+            <form class="login_window_form">
+                <input type="text">
+                <input type="text">
+            </form>
+        </div>    
     </div>
     `;
 }
+// value
 
 function xmark() {
     let backgroundLogin = document.getElementById("backgroundLogin");
 
     backgroundLogin.innerHTML = `
     <div id="backgroundLogin">
-    <div id="loginWindow">
-    </div>
+        <div id="loginWindow">
+        </div>
     </div>
     `;
+
 }
-// value
+
+
