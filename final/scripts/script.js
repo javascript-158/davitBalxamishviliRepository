fa7a23f5335f93f5ec68f9e3590b5451ba3d1cae
dato201 <146935809+dato201@users.noreply.github.com>
2024-01-18 00:09:20 +0400

    add final dan

--- final/scripts/script.js
+++ final/scripts/script.js
@@ -1,30 +1,63 @@
 function LoginButton() {
-    let backgroundLogin = document.getElementById("backgroundLogin");
-
-    backgroundLogin.innerHTML = `
-    <div id="backgroundLogin" onclick="xmark()" class="backgroundLogin">
-        <div id="loginWindow" class="login_window">
-            <button class="x" onclick="xmark()"><i class="fa-solid fa-xmark"></i></button>
-            <form class="login_window_form">
-                <input type="text">
-                <input type="text">
-            </form>
-        </div>    
-    </div>
-    `;
+    backgroundLogin.style.display = "flex";
 }
-// value
+let backgroundLogin = document.getElementById("backgroundLogin");
+
+window.onclick = function (event) {
+    if (event.target == backgroundLogin) {
+        backgroundLogin.style.display = "none";
+    }
+}
 
 function xmark() {
-    let backgroundLogin = document.getElementById("backgroundLogin");
+    backgroundLogin.style.display = "none";
+}
 
-    backgroundLogin.innerHTML = `
-    <div id="backgroundLogin">
-        <div id="loginWindow">
-        </div>
-    </div>
-    `;
+function handleRegistration() {
+    let email = document.getElementById('email').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
 
+    let oldUsersData = localStorage.getItem('usersData');
+    oldUsersData = JSON.parse(oldUsersData);
+    console.log(oldUsersData)
+
+    let user = {
+        email: email,
+        username: username,
+        password: password
+    }
+
+    if (oldUsersData === null) {
+        localStorage.setItem('usersData', JSON.stringify([user]));
+        console.log("hello world1");
+    } else {
+        for ( user of oldUsersData) {
+            if (user.username === username) {
+                let errorMessage = document.getElementById("error");
+                errorMessage.style.color = "red";
+                console.log("gudbai world");
+                errorMessage.innerHTML = "This username is already used!"
+            } else {
+                oldUsersData.push(user);
+                localStorage.setItem('usersData', JSON.stringify(oldUsersData));
+                console.log("hello world2");
+            }
+        }
+    }
 }
 
 
+
+
+
+
+// let usersData = [
+//     {
+//         email: "email",
+//         username: "username",
+//         password: "passwords"
+//     },
+// ]
+
+// localStorage.setItem("usersData", JSON.stringify(usersData))
