470a172cfd2da34235969d0f8303a1f237fb439f
dato201 <146935809+dato201@users.noreply.github.com>
2024-01-18 20:27:19 +0400

    add final dan

--- final/scripts/script.js
+++ final/scripts/script.js
@@ -1,4 +1,4 @@
-function LoginButton() {
+function signupButton() {
     backgroundLogin.style.display = "flex";
 }
 let backgroundLogin = document.getElementById("backgroundLogin");
@@ -6,6 +6,8 @@ let backgroundLogin = document.getElementById("backgroundLogin");
 window.onclick = function (event) {
     if (event.target == backgroundLogin) {
         backgroundLogin.style.display = "none";
+    } else if ((event.target == backgroundLogin2)) {
+        backgroundLogin2.style.display = "none";
     }
 }
 
@@ -28,36 +30,98 @@ function handleRegistration() {
         password: password
     }
 
-    if (oldUsersData === null) {
-        localStorage.setItem('usersData', JSON.stringify([user]));
-        console.log("hello world1");
+    let filter = /^[ ]{1,}$/gm;
+    let filter0 = /^[a-z\d._-]{1,}(@[a-z]{3,}.com)$/gm;
+
+    if (filter0.test(email) === false || filter.test(username) === true || filter.test(password) === true || username === "" || password === "") {
+        let errorMessage = document.getElementById("error");
+        errorMessage.style.color = "red";
+        errorMessage.innerHTML = "This username is already used!"
     } else {
-        for ( user of oldUsersData) {
-            if (user.username === username) {
-                let errorMessage = document.getElementById("error");
-                errorMessage.style.color = "red";
-                console.log("gudbai world");
-                errorMessage.innerHTML = "This username is already used!"
-            } else {
-                oldUsersData.push(user);
-                localStorage.setItem('usersData', JSON.stringify(oldUsersData));
-                console.log("hello world2");
-            }
+        if (oldUsersData === password) {
+            let errorMessage = document.getElementById("error");
+            errorMessage.style.color = "red";
+            errorMessage.innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAA"
+        } else {
+            oldUsersData.push(user);
+            localStorage.setItem('usersData', JSON.stringify(oldUsersData));
         }
     }
 }
 
+let usersData = [
+    {
+        email: "ggg0@gmail.com",
+        username: "username",
+        password: "passwords"
+    }
+]
 
+localStorage.setItem("usersData", JSON.stringify(usersData))
 
+function LoginButton() {
+    backgroundLogin2.style.display = "flex";
+}
+let backgroundLogin2 = document.getElementById("backgroundLogin2");
 
+function xmark2() {
+    backgroundLogin2.style.display = "none";
+}
 
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-// let usersData = [
-//     {
-//         email: "email",
-//         username: "username",
-//         password: "passwords"
-//     },
-// ]
+function generateString(length) {
+    let result = '';
+    const charactersLength = characters.length;
 
-// localStorage.setItem("usersData", JSON.stringify(usersData))
+    for (let i = 0; i < length; i++) {
+        result = result + characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function handleRegistration2() {
+    let username = document.getElementById('username2').value;
+    let password = document.getElementById('password2').value;
+    let savePasswordIsChecked = document.getElementById('savePassword').checked;
+
+    let usersData = localStorage.getItem('usersData');
+    usersData = JSON.parse(usersData);
+
+    let userExists = false;
+
+    let loginForm = document.getElementById('loginWindowForm')
+
+    // let timeNow = new Date();
+
+    // timeNow.setTime(timeNow.getTime() + (5 * 24 * 60 * 1000));
+
+    // let expires = "expires=" + timeNow.toUTCString();
+    let sessionToken = generateString(36);
+    console.log(expires);
+//${expires};
+    for (let user = 0; user < usersData.length; user++) {
+        if (usersData[user].username === username && usersData[user].password === password) {
+            if (savePasswordIsChecked === true) {
+                document.cookie = `sessionToken=${sessionToken}; path=/`;
+            } else {
+                sessionStorage.setItem('sessionToken', sessionToken);
+            }
+
+            usersData[user].sessionToken = sessionToken;
+            userExists = true;
+        }
+    }
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+    });
+
+    if (userExists === false) {
+        window.location.href = 'loginFailed.html';
+    } else {
+        window.location.href = 'loginSuccess.html';
+    }
+
+    localStorage.setItem('usersData', JSON.stringify(usersData));
+}
